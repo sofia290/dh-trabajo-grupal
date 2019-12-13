51b12347f235c4e01ced8decf08e544d750c4e22
CamilaChancalay <55936333+CamilaChancalay@users.noreply.github.com>
2019-12-13 13:11:35 -0300

    Update BD.php

--- clases/BD.php
+++ clases/BD.php
@@ -6,7 +6,7 @@ class BD{
   public function __construct(){
       $dsn = 'mysql:host=localhost;dbname=ecopreguntas;port=3306';
       $db_user = 'root';
-      $db_pass = '';
+      $db_pass = 'root';
       $opt = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
       $this->con = new PDO($dsn, $db_user, $db_pass, $opt);
     }
