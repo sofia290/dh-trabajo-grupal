8b3c3e23551917bfaf66e1887f1edd6092bc984d
sofia290 <43052291+sofia290@users.noreply.github.com>
2019-12-13 13:39:41 -0300

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,11 @@
               $username = $bd->mostrarUsername($_SESSION["user_id"]);
               ?>
               <li><a href="logout.php"> Cerrar sesion </a></li>
-              <li><?= $username  ?>  </li>
+              <li><?= $username  ?> 
+                <ul>
+                  <li> <a href="editarPerfil.php"> Editar perfil</a></li>
+                </ul>
+            </li>
               <?php
             }
             ?>
